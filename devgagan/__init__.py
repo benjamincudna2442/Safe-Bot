5cf6dfcafe7da74a2174c160f75d4998f717a593
benjamincudna2442 <customsmsbd72838@gmail.com>
2025-06-09 18:46:23 +0600

    Update __init__.py

--- devgagan/__init__.py
+++ devgagan/__init__.py
@@ -1,17 +1,3 @@
-# ---------------------------------------------------
-# File Name: __init__.py
-# Description: A Pyrogram bot for downloading files from Telegram channels or groups 
-#              and uploading them back to Telegram.
-# Author: Gagan
-# GitHub: https://github.com/devgaganin/
-# Telegram: https://t.me/team_spy_pro
-# YouTube: https://youtube.com/@dev_gagan
-# Created: 2025-01-11
-# Last Modified: 2025-01-11
-# Version: 2.0.5
-# License: MIT License
-# ---------------------------------------------------
-
 import asyncio
 import logging
 import time
@@ -21,13 +7,15 @@ from config import API_ID, API_HASH, BOT_TOKEN, STRING, MONGO_DB, DEFAULT_SESSIO
 from telethon.sync import TelegramClient
 from motor.motor_asyncio import AsyncIOMotorClient
 
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+    datefmt="%Y-%m-%d %H:%M:%S"
+)
+logger = logging.getLogger(__name__)
+
 loop = asyncio.new_event_loop()
 asyncio.set_event_loop(loop)
-
-logging.basicConfig(
-    format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s",
-    level=logging.INFO,
-)
 
 botStartTime = time.time()
 
@@ -40,13 +28,15 @@ app = Client(
     parse_mode=ParseMode.MARKDOWN
 )
 
+logger.info("Creating Telethon Bot Client From BOT_TOKEN")
 sex = TelegramClient('sexrepo', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
+logger.info("Telethon Bot Client Created Successfully")
 
+logger.info("Creating Pyro Bot Client From BOT_TOKEN")
 if STRING:
     pro = Client("ggbot", api_id=API_ID, api_hash=API_HASH, session_string=STRING)
 else:
     pro = None
-
 
 if DEFAULT_SESSION:
     userrbot = Client("userrbot", api_id=API_ID, api_hash=API_HASH, session_string=DEFAULT_SESSION)
@@ -54,20 +44,19 @@ else:
     userrbot = None
 
 telethon_client = TelegramClient('telethon_session', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
+logger.info("Pyro Bot Client Created Successfullly")
 
-# MongoDB setup
+logger.info("Creating Mongo Client From MONGO_DB")
 tclient = AsyncIOMotorClient(MONGO_DB)
-tdb = tclient["telegram_bot"]  # Your database
-token = tdb["tokens"]  # Your tokens collection
+tdb = tclient["telegram_bot"]
+token = tdb["tokens"]
+logger.info("Mongo Client Created Successfully")
 
 async def create_ttl_index():
-    """Ensure the TTL index exists for the `tokens` collection."""
     await token.create_index("expires_at", expireAfterSeconds=0)
 
-# Run the TTL index creation when the bot starts
 async def setup_database():
     await create_ttl_index()
-    print("MongoDB TTL index created.")
 
 async def restrict_bot():
     global BOT_ID, BOT_NAME, BOT_USERNAME
@@ -82,5 +71,6 @@ async def restrict_bot():
         await pro.start()
     if userrbot:
         await userrbot.start()
+    logger.info("RestrictedContentDL Successfully Started 💥")
 
 loop.run_until_complete(restrict_bot())
