77855654e2f12bbfca294009dfd96ab5c3a3f237
benjamincudna2442 <customsmsbd72838@gmail.com>
2025-06-09 19:29:30 +0600

    Update shrink.py

--- devgagan/modules/shrink.py
+++ devgagan/modules/shrink.py
@@ -1,3 +1,4 @@
+```python
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram.enums import ParseMode
@@ -33,8 +34,6 @@ async def token_handler(client, message):
     join = await subscribe(client, message)
     if join == 1:
         return
-    chat_id = "TheSmartDev"
-    msg = await app.get_messages(chat_id, 2595)
     user_id = message.chat.id
     user = await app.get_users(user_id)
     fullname = f"{user.first_name} {user.last_name}" if user.last_name else user.first_name
@@ -45,34 +44,20 @@ async def token_handler(client, message):
                 InlineKeyboardButton("Source Repo", url="https://github.com/abirxdhack/RestrictedContentDL")
             ],
             [
-                InlineKeyboardButton("Help Menu", b"help"),
-                InlineKeyboardButton("Close Menu", b"close")
+                InlineKeyboardButton("Help Menu", b"$starthelpmsg"),
+                InlineKeyboardButton("Close Menu", b"$closeStartmsg")
             ]
         ]
         keyboard = InlineKeyboardMarkup(buttons)
-        if msg.photo:
-            await message.reply_photo(
-                msg.photo.file_id,
-                caption=(
-                    f"**Hi {fullname}! Welcome To This Bot**\n"
-                    "**━━━━━━━━━━━━━━━━━━━━━━**\n"
-                    "**RestrictedDL ⚙️:** The ultimate toolkit on Telegram, offering Downloading Any Type Of Resticted Content From Both Public & Private Source!\n"
-                    "**━━━━━━━━━━━━━━━━━━━━━━**\n"
-                    "**Don't Forget To [Join Here](https://t.me/TheSmartDev) For Updates!**"
-                ),
-                reply_markup=keyboard,
-                parse_mode=ParseMode.MARKDOWN
-            )
-        else:
-            await message.reply(
-                f"**Hi {fullname}! Welcome To This Bot**\n"
-                "**━━━━━━━━━━━━━━━━━━━━━━**\n"
-                "**RestrictedDL ⚙️:** The ultimate toolkit on Telegram, offering Downloading Any Type Of Resticted Content From Both Public & Private Source!\n"
-                "**━━━━━━━━━━━━━━━━━━━━━━**\n"
-                "**Don't Forget To [Join Here](https://t.me/TheSmartDev) For Updates!**",
-                reply_markup=keyboard,
-                parse_mode=ParseMode.MARKDOWN
-            )
+        await message.reply(
+            f"**Hi {fullname}! Welcome To This Bot**\n"
+            "**━━━━━━━━━━━━━━━━━━━━━━**\n"
+            "**RestrictedDL ⚙️:** The ultimate toolkit on Telegram, offering Downloading Any Type Of Resticted Content From Both Public & Private Source!\n"
+            "**━━━━━━━━━━━━━━━━━━━━━━**\n"
+            "**Don't Forget To [Join Here](https://t.me/TheSmartDev) For Updates!**",
+            reply_markup=keyboard,
+            parse_mode=ParseMode.MARKDOWN
+        )
         return
 
     param = message.command[1] if len(message.command) > 1 else None
@@ -118,12 +103,10 @@ async def smart_handler(client, message):
         )
         await message.reply("Click the button below to verify your free access token: \n\n> What will you get ? \n1. No time bound upto 3 hours \n2. Batch command limit will be FreeLimit + 20 \n3. All functions unlocked", reply_markup=button)
 
-@app.on_callback_query(filters.regex(b"help"))
+@app.on_callback_query(filters.regex(r"^\$starthelpmsg$"))
 async def help_callback(client, callback_query):
     user_id = callback_query.from_user.id
-    chat_id = "TheSmartDev"
-    msg = await app.get_messages(chat_id, 2595)
-    buttons = [[InlineKeyboardButton("Back", b"back")]]
+    buttons = [[InlineKeyboardButton("Back", b"$backStartmsg")]]
     keyboard = InlineKeyboardMarkup(buttons)
     help_text = (
         "**📝 Bot Commands Overview:**\n\n"
@@ -148,62 +131,38 @@ async def help_callback(client, callback_query):
         "19. **/tera** or **/terabox link** - Download from TeraBox\n"
         "20. **/activate** - Activate downloader for private chats (5 min)\n"
     )
-    if msg.photo:
-        await callback_query.message.edit_media(
-            media=msg.photo,
-            caption=help_text,
-            reply_markup=keyboard,
-            parse_mode=ParseMode.MARKDOWN
-        )
-    else:
-        await callback_query.message.edit_text(
-            help_text,
-            reply_markup=keyboard,
-            parse_mode=ParseMode.MARKDOWN
-        )
+    await callback_query.message.edit_text(
+        help_text,
+        reply_markup=keyboard,
+        parse_mode=ParseMode.MARKDOWN
+    )
 
-@app.on_callback_query(filters.regex(b"back"))
+@app.on_callback_query(filters.regex(r"^\$backStartmsg$"))
 async def back_callback(client, callback_query):
     user_id = callback_query.from_user.id
     user = await app.get_users(user_id)
     fullname = f"{user.first_name} {user.last_name}" if user.last_name else user.first_name
-    chat_id = "TheSmartDev"
-    msg = await app.get_messages(chat_id, 2595)
     buttons = [
         [
             InlineKeyboardButton("Update Channel", url="https://t.me/TheSmartDev"),
             InlineKeyboardButton("Source Repo", url="https://github.com/abirxdhack/RestrictedContentDL")
         ],
         [
-            InlineKeyboardButton("Help Menu", b"help"),
-            InlineKeyboardButton("Close Menu", b"close")
+            InlineKeyboardButton("Help Menu", b"$starthelpmsg"),
+            InlineKeyboardButton("Close Menu", b"$closeStartmsg")
         ]
     ]
     keyboard = InlineKeyboardMarkup(buttons)
-    if msg.photo:
-        await callback_query.message.edit_media(
-            media=msg.photo,
-            caption=(
-                f"**Hi {fullname}! Welcome To This Bot**\n"
-                "**━━━━━━━━━━━━━━━━━━━━━━**\n"
-                "**RestrictedDL ⚙️:** The ultimate toolkit on Telegram, offering Downloading Any Type Of Resticted Content From Both Public & Private Source!\n"
-                "**━━━━━━━━━━━━━━━━━━━━━━**\n"
-                "**Don't Forget To [Join Here](https://t.me/TheSmartDev) For Updates!**"
-            ),
-            reply_markup=keyboard,
-            parse_mode=ParseMode.MARKDOWN
-        )
-    else:
-        await callback_query.message.edit_text(
-            f"**Hi {fullname}! Welcome To This Bot**\n"
-            "**━━━━━━━━━━━━━━━━━━━━━━**\n"
-            "**RestrictedDL ⚙️:** The ultimate toolkit on Telegram, offering Downloading Any Type Of Resticted Content From Both Public & Private Source!\n"
-            "**━━━━━━━━━━━━━━━━━━━━━━**\n"
-            "**Don't Forget To [Join Here](https://t.me/TheSmartDev) For Updates!**",
-            reply_markup=keyboard,
-            parse_mode=ParseMode.MARKDOWN
-        )
+    await callback_query.message.edit_text(
+        f"**Hi {fullname}! Welcome To This Bot**\n"
+        "**━━━━━━━━━━━━━━━━━━━━━━━━━━━━**\n"
+        "**RestrictedDL ⚙️:** The ultimate toolkit on Telegram, offering Downloading Any Type Of Resticted Content From Both Public & Private Source!\n"
+        "**━━━━━━━━━━━━━━━━━━━━━━**\n"
+        "**Don't forget To [Join Here](https://t.me/TheSmartDev) For Updates!**",
+        reply_markup=keyboard,
+        parse_mode=ParseMode.MARKDOWN
+    )
 
-@app.on_callback_query(filters.regex(b"close"))
+@app.on_callback_query(filters.regex(r"^\$closeStartmsg$"))
 async def close_callback(client, callback_query):
     await callback_query.message.delete()
